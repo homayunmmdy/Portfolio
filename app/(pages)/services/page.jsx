1ed24d0e87a735056f6bec1d6460c0b7c983fe29
Homayoun <homayoun763@gmail.com>
2024-05-02 16:32:01 +0330

    add services

--- app/(pages)/services/page.jsx
+++ app/(pages)/services/page.jsx
@@ -1,16 +1,18 @@
 import PageDescription from '@/app/components/PageDescription'
 import PageTitle from '@/app/components/PageTitle'
+import Services from '@/app/components/Services'
 import React from 'react'
 
-const Services = () => {
+const ServicesPage = () => {
   return (
     <div>
       <PageTitle titr="services" title="High-Impact Services" />
       <div className='px-5 w-[98%] md:w-[92%] mx-auto'>
         <PageDescription title="High-impact services to help your business" description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias eos quas blanditiis, quos sint nostrum fugit aperiam inventore optio itaque molestias corporis, ipsa tenetur eligendi nihil iste porro, natus culpa consequuntur? Maxime, corporis tempore. Sed tenetur veritatis velit recusandae eum, molestiae voluptate ducimus laudantium esse illo doloribus atque eligendi deleniti iusto." />
+        <Services />
       </div>
     </div>
   )
 }
 
-export default Services
+export default ServicesPage
---
+++ app/components/Services.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import Service from './Service'
+
+const data = [
+    {
+        id: 1,
+        title: "Frontend Devloper",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus iste ipsam quod repellat. Hic tempora ullam aperiam ipsum optio magni vel inventore voluptatibus nisi maiores laboriosam fuga iure, velit eligendi ab vero minima? Quae ducimus ab dignissimos iure, eos consequatur est deleniti cum id aliquid neque.",
+        items: ["one", "two" , "three"]
+    },
+    {
+        id: 2,
+        title: "Frontend Devloper",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus iste ipsam quod repellat. Hic tempora ullam aperiam ipsum optio magni vel inventore voluptatibus nisi maiores laboriosam fuga iure, velit eligendi ab vero minima? Quae ducimus ab dignissimos iure, eos consequatur est deleniti cum id aliquid neque.",
+    },
+    {
+        id: 3,
+        title: "Frontend Devloper",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus iste ipsam quod repellat. Hic tempora ullam aperiam ipsum optio magni vel inventore voluptatibus nisi maiores laboriosam fuga iure, velit eligendi ab vero minima? Quae ducimus ab dignissimos iure, eos consequatur est deleniti cum id aliquid neque.",
+    },
+    {
+        id: 4,
+        title: "Frontend Devloper",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus iste ipsam quod repellat. Hic tempora ullam aperiam ipsum optio magni vel inventore voluptatibus nisi maiores laboriosam fuga iure, velit eligendi ab vero minima? Quae ducimus ab dignissimos iure, eos consequatur est deleniti cum id aliquid neque.",
+    },
+    {
+        id: 5,
+        title: "Frontend Devloper",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus iste ipsam quod repellat. Hic tempora ullam aperiam ipsum optio magni vel inventore voluptatibus nisi maiores laboriosam fuga iure, velit eligendi ab vero minima? Quae ducimus ab dignissimos iure, eos consequatur est deleniti cum id aliquid neque.",
+    },
+    {
+        id: 6,
+        title: "Frontend Devloper",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus iste ipsam quod repellat. Hic tempora ullam aperiam ipsum optio magni vel inventore voluptatibus nisi maiores laboriosam fuga iure, velit eligendi ab vero minima? Quae ducimus ab dignissimos iure, eos consequatur est deleniti cum id aliquid neque.",
+    },
+]
+const Services = () => {
+    return (
+        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6'>
+            {data?.map(item => (
+                <Service key={item.id} number={item.id} title={item.title} desc={item.description} items={item.items}/>
+            ))}
+        </div>
+    )
+}
+
+export default Services
