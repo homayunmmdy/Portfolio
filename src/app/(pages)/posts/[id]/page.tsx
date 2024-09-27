c393b72339f5960fe34fc0535a192fa6592f9661
Homayoun <homayoun763@gmail.com>
2024-09-27 22:06:03 +0330

    replace read with back button

--- src/app/(pages)/posts/[id]/page.tsx
+++ src/app/(pages)/posts/[id]/page.tsx
@@ -1,17 +1,13 @@
 "use client";
 import { PageTitle } from "@/components";
 import ExpandableText from "@/components/ExpandableText";
-import useReadText from "@/hooks/useReadText";
 import useSinglePost from "@/hooks/useSinglePost";
 import Image from "next/image";
 import React from "react";
-import { FaPlay, FaStop } from "react-icons/fa";
 
 const SinglePostPage = () => {
   //@ts-ignore
   const post = useSinglePost();
-  //@ts-ignore
-  const { isSpeaking, handleReadText, handleStopReading } = useReadText(post?.body);
   return (
     <>
       {/* @ts-ignore */}
@@ -31,21 +27,29 @@ const SinglePostPage = () => {
             <div className="text-center gap-4 items-center flex-col">
               {/* @ts-ignore */}
               <ExpandableText text={post?.body} />
-              {!isSpeaking ? (
-                <button
-                  className="btn btn-neutral text-white rounded-full ml-3"
-                  onClick={handleReadText}
-                >
-                  <FaPlay />
-                </button>
-              ) : (
-                <button
-                  className="btn btn-neutral text-white rounded-full ml-3"
-                  onClick={handleStopReading}
-                >
-                  <FaStop />
-                </button>
-              )}
+              <button
+                className=" text-center w-48 rounded-2xl h-14 relative  text-xl font-semibold group"
+                type="button"
+              >
+                <div className="bg-black rounded-xl h-12 w-1/4 flex items-center justify-center absolute left-1 top-[4px] group-hover:w-[184px] z-10 duration-500">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 1024 1024"
+                    height="25px"
+                    width="25px"
+                  >
+                    <path
+                      d="M224 480h640a32 32 0 1 1 0 64H224a32 32 0 0 1 0-64z"
+                      fill="#000000"
+                    ></path>
+                    <path
+                      d="m237.248 512 265.408 265.344a32 32 0 0 1-45.312 45.312l-288-288a32 32 0 0 1 0-45.312l288-288a32 32 0 1 1 45.312 45.312L237.248 512z"
+                      fill="#000000"
+                    ></path>
+                  </svg>
+                </div>
+                <p className="translate-x-2">Go Back</p>
+              </button>
             </div>
           </p>
         </div>
